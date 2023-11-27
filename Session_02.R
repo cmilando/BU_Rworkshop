a46a178136a44de06fbe20f690de8d69a274bfc2
cmilando <44262671+cmilando@users.noreply.github.com>
2023-11-27 16:40:50 -0500

    Initial commit

---
+++ Session_02.R
@@ -0,0 +1,272 @@
+# ============================================================================
+# Author: CWM
+# Date: 6/25/20
+# Purpose: Session 2 code notes, created automatically with knitr::purl(*.Rmd)
+# ============================================================================
+
+## ----setup, include=FALSE----
+knitr::opts_chunk$set(echo = T)
+knitr::opts_chunk$set(size = 'scriptsize')
+
+
+## ----read-in----------------
+aqs_data <- read.csv('aqs_data.csv')
+
+
+## ----eval=F-----------------
+## library(haven) # part of tidyverse
+## raw <- read_sas("../SCC db/brfss2012.sas7bdat")
+
+
+## ----eval=F-----------------
+## library(readxl) # part of tidyverse
+## read_excel("my_excel_file.xlsx")
+
+
+## ----eval=F-----------------
+## library(rgdal)  # for shapefiles and spatial data
+## library(raster) # for the library of projections
+## PUMA_USA <- readOGR(dsn = 'ipums_puma_2010.shp')
+## PUMA_MA <- subset_sp(PUMA_USA, STATEFIP == 25)
+
+
+## ---------------------------
+# ======================================================
+# Author: CWM
+# Date: 6/24/2020
+# Purpose: Scripts to use for R coding session
+# ======================================================
+
+
+## ---- message=F, eval = F----
+## library(tidyverse)
+##
+## # read in file
+## aqs_data <- read.csv('aqs_data.csv')
+##
+## # --------------------------------------------- (... to 80)
+## # Section 1: find max
+##
+## ## ...
+## <code>
+
+
+## ---------------------------
+aqs_data_wide <- read.csv('wide_fmt.csv')
+
+
+## ---------------------------
+# need a new function that handles missing values
+get_max <- function(x) max(x, na.rm = T)
+
+# starting at column 2 because 1st column is Date
+get_max(aqs_data_wide[, 2])
+
+# for each column 2 to the end
+for (j in 2:ncol(aqs_data_wide)) {
+  print(get_max(aqs_data_wide[, j]))
+}
+
+
+## ---------------------------
+apply(X = aqs_data_wide[, -1],  # the matrix
+      MARGIN = 2,               # (2) means columns, (1) means rows
+      FUN = get_max)            # function to "apply" to each row or col
+
+
+
+## ---------------------------
+my_list <- vector('list', length = 10)
+site_IDs <- unique(aqs_data$Site.ID)
+for (i in 1:length(site_IDs)) {
+  my_list[[i]] <- subset(aqs_data, Site.ID == site_IDs[i],
+                         select = Daily.Mean.PM2.5.Concentration)
+}
+
+lapply(my_list, get_max)
+
+
+## ---------------------------
+library(parallel)
+detectCores()
+
+
+## ---------------------------
+get_max <- function(x) max(x, na.rm = T)
+
+## Loop 1
+set.seed(1) # if you randomize, use this for reproducability
+bs_rows <- sample(x = 1:nrow(aqs_data_wide), size = 500, replace = T)
+head(apply(X = aqs_data_wide[bs_rows, -1], 2, get_max))
+
+## Loop 2
+set.seed(2)
+bs_rows <- sample(x = 1:nrow(aqs_data_wide), size = 500, replace = T)
+head(apply(X = aqs_data_wide[bs_rows, -1], 2, get_max))
+
+
+
+## ---------------------------
+get_bs_max <- function(loop_i) {
+  set.seed(loop_i)
+  bs_rows <- sample(x = 1:nrow(aqs_data_wide), size = 500, replace = T)
+  bs_max <- apply(X = aqs_data_wide[bs_rows, -1], 2, get_max)
+  return(bs_max)
+}
+head(get_bs_max(2)) # matches previous slide!
+
+
+
+## ---- message=F-------------
+library(foreach)
+library(doParallel)
+seeds <- 1:5
+
+# this tells R the number of cores
+registerDoParallel(detectCores())
+
+# this uses rbind, function that combines all outputs together
+foreach (i = seeds, .combine=rbind) %dopar% {
+  get_bs_max(i)
+}
+
+# close the cluster when done
+stopImplicitCluster()
+
+
+## ---------------------------
+head(aqs_data[, 1:6])
+
+
+## ---- message = F-----------
+library(tidyverse)
+aqs_data %>%
+  group_by(Site.ID, POC) %>%  # similar to looping over matrix columns
+  summarize(site_max = max(Daily.Mean.PM2.5.Concentration))
+
+
+## ---- echo=F----------------
+t(aqs_data[1, ])
+
+
+## ---------------------------
+wide_fmt <- aqs_data %>%
+  pivot_wider(id_cols = c(Site.ID, Date, POC),
+              names_from = c(Site.ID, POC),
+              values_from = Daily.Mean.PM2.5.Concentration)
+
+
+## ---- eval = F--------------
+##   aqs_data %>%
+##     group_by(Site.ID, POC, ...) # other constraints
+##     summarize(max = max(val),
+##               min = min(val), ...) # other summary stats
+
+
+## ---- fig.height = 3--------
+# libary(ggplot) is not necessary, part of tidyverse
+
+# add a shorter conc column
+aqs_data$PM25 <- aqs_data$Daily.Mean.PM2.5.Concentration
+
+ggplot(data = aqs_data) +
+  geom_boxplot(mapping = aes(x = PM25), color = 'blue')
+
+
+## ---- eval = F--------------
+## ggplot(data = aqs_data) +
+
+
+## ---- eval = F--------------
+## geom_boxplot(mapping = aes(x = PM25), color = 'blue')
+
+
+## ---- fig.height = 3.5------
+ggplot(data = aqs_data) +
+  geom_boxplot(aes(x = PM25, y = Site.Name), color = 'blue')
+
+
+## ---- fig.height = 3.5------
+ggplot(data = aqs_data) +
+  geom_boxplot(aes(x = PM25, y = Site.Name, fill = COUNTY),
+               color = 'blue')
+
+
+## ---- fig.height=3----------
+ggplot(data = aqs_data) +
+  geom_boxplot(aes(x = PM25, y = Site.Name, fill = COUNTY),
+               color = 'black') +
+  theme(panel.background = element_rect(fill = 'white')) +
+  coord_cartesian(xlim = c(2, 15))
+
+
+## ----echo = F---------------
+library(RColorBrewer)
+
+
+## ---- fig.height=3----------
+ggplot(data = aqs_data) +
+  geom_boxplot(aes(x = PM25, y = Site.Name, fill = COUNTY),
+               color = 'black') +
+  theme(panel.background = element_rect(fill = 'white')) +
+  coord_cartesian(xlim = c(2, 15)) +
+  scale_fill_brewer(palette = 'Paired')
+
+
+## ---- echo = F, message = F----
+library(tools)
+
+
+## ---- message=F, eval = F----
+## ggplot(data = aqs_data) +
+##   geom_boxplot(aes(x = PM25, y = toTitleCase(tolower(Site.Name)),
+##                    fill = COUNTY), color = 'black') +
+##   ylab(NULL) +
+##   xlab(expression(PM[2.5]~(mu*g/m^3))) + #modify x axis title
+##   theme_classic(base_size = 12, base_family = 'serif') +
+##   theme(axis.text.y = element_text(hjust = 0)) +
+##   coord_cartesian(xlim = c(2, 15)) +
+##   scale_fill_brewer(palette = 'Paired')
+
+
+## ---- echo = F, fig.height=4----
+ggplot(data = aqs_data) +
+  geom_boxplot(aes(x = PM25, y = toTitleCase(tolower(Site.Name)),
+                   fill = COUNTY), color = 'black') +
+  ylab(NULL) +
+  xlab(expression(PM[2.5]~(mu*g/m^3))) + #modify x axis title
+  theme_classic(base_size = 14, base_family = 'serif') +
+  theme(axis.text.y = element_text(hjust = 0)) +
+  coord_cartesian(xlim = c(2, 15)) +
+  scale_fill_brewer(palette = 'Paired')
+
+
+## ---- fig.height=3.5--------
+ggplot(data = subset(aqs_data, COUNTY == 'Essex')) +
+  geom_boxplot(aes(x = factor(POC), y = PM25, fill = factor(POC)),
+               color = 'black') +
+  theme_bw() +
+  facet_grid(~ Site.Name) # rows ~ column factors
+
+
+## ---- echo = F, message = F----
+library(gridExtra)
+
+
+## ---- fig.height = 4--------
+p1 <- ggplot(iris) + geom_boxplot(aes(x = Species, y = Sepal.Length))
+p2 <- ggplot(iris) + stat_density(aes(x = Petal.Length, fill = Species))
+
+grid.arrange(p1, p2, nrow = 1, widths = c(1, 1.5))
+
+
+## ---- eval = F--------------
+## p1 <- ggplot(iris) + geom_boxplot(aes(x = Species, y = Sepal.Length))
+## p2 <- ggplot(iris) + stat_density(aes(x = Petal.Length, fill = Species))
+##
+## final <- grid.arrange(p1, p2, nrow = 1, widths = c(1, 1.5))
+##
+## ggsave(filename = "final.png", plot = final, width = 8, height = 3,
+##        units = "in", dpi = 300)
+##
+
---
+++ session_01.R
@@ -0,0 +1,285 @@
+# ============================================================================
+# Author: CWM
+# Date: 6/25/20
+# Purpose: Session 1 code notes, created automatically with knitr::purl(*.Rmd)
+# ============================================================================
+
+## ----setup, include=FALSE----
+knitr::opts_chunk$set(echo = T)
+knitr::opts_chunk$set(size = 'scriptsize')
+
+
+## ----read-in----------------
+aqs_data <- read.csv('aqs_data.csv')
+
+
+## ----dim--------------------
+dim(aqs_data)
+
+
+## ----view, eval=F-----------
+## View(aqs_data)
+
+
+## ---------------------------
+aqs_data[1, 2]
+
+
+## ---------------------------
+aqs_data[ c(1, 2), c(1, 2)]
+
+
+## ---------------------------
+typeof(2.0)
+
+
+## ---------------------------
+object.size(2.0)
+
+
+## ---------------------------
+object.size(TRUE)
+
+
+## ---------------------------
+object.size("2")
+
+
+## ---------------------------
+x <- c(4, 5)
+x[1]
+
+
+## ---- eval = F--------------
+## aqs_data[ c(1, 2), c(1, 2)]
+
+
+## ---- eval=F----------------
+## head(aqs_data)
+
+
+## ----message=F--------------
+library(tidyverse)
+
+aqs_data %>% glimpse()
+
+
+## ---------------------------
+animal_type <- c('pig', 'pig', 'chicken', 'horse')
+farm_animals <- rep(animal_type, times = 100) # repeat 100x
+animals_as_factor <- factor(farm_animals)    # convert to factor
+#object.size(farm)                # 3416 bytes
+#object.size(farm_as_factor)      # 2232 bytes
+head(as.integer(animals_as_factor))  # internally alphabetical
+
+
+## ---------------------------
+# animals <- c('pig', 'pig', 'chicken', 'horse')
+animal_int_score <- c(10, 10, 2, 8)
+farm_int <- rep(animal_int_score, 100) + rnorm(n = 400)
+coef(lm(farm_int ~ farm_animals))        # as character
+coef(lm(farm_int ~ animals_as_factor))   # as factor, same as above
+coef(lm(farm_int ~ as.integer(animals_as_factor))) # different
+
+
+## ---------------------------
+typeof(aqs_data)
+
+
+## ---------------------------
+sites <- unique(aqs_data$Site.Name) # gets unique sites
+sites[1]
+
+# STEP 1. Get data from a single station
+site_1 <- subset(aqs_data, Site.Name == sites[1])
+
+
+## ---- eval = F--------------
+## 1 == 1    # is the left side 'is equal to' the right side? T
+## 1 <= 2    # is the left side 'less than or equal to' the right side? T
+## 1 != "1"  # is the left side 'not equal to' the right side
+
+
+## ---------------------------
+# STEP 2: Find max
+max_site_1 <- max(site_1$Daily.Mean.PM2.5.Concentration)
+
+
+## ---------------------------
+# STEP 3: print max
+paste('Max value for ', sites[2], 'was', max_site_1, 'ug/m3')
+
+
+## ---------------------------
+# STEP 1. Get data from a single station
+site_2 <- subset(aqs_data, Site.Name == sites[2])
+# STEP 2: Find max
+max_site_2 <- max(site_2$Daily.Mean.PM2.5.Concentration)
+# STEP 3: print max
+paste('Max value for', sites[2], 'was', max_site_1, 'ug/m3')
+
+
+## ---------------------------
+a <- 1
+b <- 2
+add <- function(arg1, arg2) {
+  arg1 + arg2 # could also do return(arg1 + arg2)
+}
+add(a, b) # you call a function using the ()
+
+
+## ---------------------------
+a <- 1
+add <- function(arg1, arg2) {
+  a <- 3
+  return(arg1 + arg2)
+}
+add(a, 2) # function is called
+a         # unchanged
+
+
+## ---- eval = F--------------
+## get_site_max <- function(  ) {
+##   # STEP 1. Get data from a single station
+##   site_x <- subset(aqs_data, Site.Name == sites[  ])
+##   # STEP 2: Find max
+##   max_site_x <- max(site_x$Daily.Mean.PM2.5.Concentration)
+##   # STEP 3: print max
+##   paste('Max value for', sites[  ], 'was', max_site_x, 'ug/m3')
+## }
+
+
+## ---- error=T---------------
+get_site_max <- function(i) {
+  # STEP 1. Get data from a single station
+  site_x <- subset(aqs_data, Site.Name == sites[i])
+  # STEP 2: Find max
+  max_site_x <- max(site_x$Daily.Mean.PM2.5.Concentration)
+  # STEP 3: print max
+  paste('Max value for', sites[i], 'was', max_site_x, 'ug/m3')
+}
+get_site_max(i = 1)
+max_site_i # doesnt exist in global environment
+
+
+## ---------------------------
+length(unique(aqs_data$Site.Name))
+
+
+## ---------------------------
+for(i in 1:3) { # could be 1:11, just wanted to minimize printing
+  print(get_site_max(i = i))
+}
+
+
+## ---- eval = F--------------
+## for(i in 1:11) {
+##   print(get_site_max(i = i))
+## }
+
+
+## ---------------------------
+get_site_max_date <- function(i) {
+  # STEP 1. Get data from a single station
+  site_x <- subset(aqs_data, Site.Name == sites[i])
+  # STEP 2: Find max
+  max_site_x <- max(site_x$Daily.Mean.PM2.5.Concentration)
+  # >>>: Which date was this
+  row_of_max <- which(site_x$Daily.Mean.PM2.5.Concentration == max_site_x)
+  date_of_max <- site_x$Date[row_of_max]
+  # STEP 3: print date of max
+  paste('Date of max value for', sites[i], 'was', date_of_max)
+}
+get_site_max_date(i = 1)
+
+
+## ---------------------------
+typeof(aqs_data$Date[1]) # 05/27/2016
+
+
+## ---------------------------
+test_date   <- '05/27/2016'
+strptime(x = test_date, format = '%m/%d/%Y' , tz = 'America/New_York')
+
+
+## ---------------------------
+test_date   <- '05/27/2016'
+formatted_date <- strptime(x = test_date,
+                          format = '%m/%d/%Y',
+                          tz = 'America/New_York')
+strftime(formatted_date, format = '%A')
+
+
+## ---------------------------
+aqs_data$Date_formatted <- strptime(x = aqs_data$Date,
+                          format = '%m/%d/%Y',
+                          tz = 'America/New_York')
+aqs_data$day_of_week <- strftime(aqs_data$Date_formatted, format = '%A')
+head(aqs_data$day_of_week)
+
+
+## ---------------------------
+get_site_max_date <- function(i) {
+  # STEP 1. Get data from a single station
+  site_x <- subset(aqs_data, Site.Name == sites[i])
+  # STEP 2: Find max
+  max_site_x <- max(site_x$Daily.Mean.PM2.5.Concentration)
+  # >>>: Which date was this
+  row_of_max <- which(site_x$Daily.Mean.PM2.5.Concentration == max_site_x)
+  weekday_of_max <- site_x$day_of_week[row_of_max]
+  # STEP 3: print date of max
+  paste('Weekday value for', sites[i], 'was', weekday_of_max)
+}
+get_site_max_date(i = 1)
+
+
+## ---------------------------
+worst_weekdays <- vector(mode = "list", length = 11)
+
+length(worst_weekdays)
+worst_weekdays[[1]]    # extra bracket for indexing into lists
+worst_weekdays %>% glimpse()
+
+
+## ---------------------------
+get_site_p95_date <- function(i) {
+  # STEP 1. Get data from a single station
+  site_x <- subset(aqs_data, Site.Name == sites[i])
+  # STEP 2: Find top 95% value
+  p95_site_x <- quantile(site_x$Daily.Mean.PM2.5.Concentration, 0.95)
+  # >>>: Which date was at or above the 95th percentile
+  rows_p95 <- which(site_x$Daily.Mean.PM2.5.Concentration >= p95_site_x)
+  weekdays_of_max <- site_x$day_of_week[rows_p95]
+  # STEP 3: return weekdays of at or above the 95th percentile
+  return(weekdays_of_max)
+}
+get_site_p95_date(1)
+
+
+## ---------------------------
+worst_weekdays <- vector(mode = "list", length = 11)
+
+for(i in 1:11) {
+  worst_weekdays[[i]] <- get_site_p95_date(i)
+}
+
+worst_weekdays %>% glimpse()
+
+
+## ---- eval=F----------------
+## # one by one?
+## worst_weekdays_vec <- c(worst_weekdays[[1]], worst_weekdays[[2]], ...)
+
+
+## ---------------------------
+# all together!
+worst_weekdays_vec <- do.call(what = c, args = worst_weekdays)
+
+
+## ---------------------------
+table(worst_weekdays_vec)
+
+
+## ---------------------------
+barplot.default(table(worst_weekdays_vec))
+
